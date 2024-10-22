4542fc5d421c97a5becd8df77cd884a4ded90e85
Rakshanda@26 <rakshandamm123@gmail.com>
2024-10-22 19:58:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rakshanda",
+    author_email="rakshandamm123@gmail.com",
+    packages=find_packages()
+)
